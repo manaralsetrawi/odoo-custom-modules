af1da54dadd5e16fd0e41208e50d1000b722c722
Manar Alsetrawi <202200412@student.polytechnic.bh>
2026-02-20 17:38:52 +0300

    Creating HR Employee Documents module

---
+++ hr_employee_documents/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    "name": "HR Employee Documents",
+    "version": "1.0",
+    "summary": "Attach documents to employees",
+    "category": "Human Resources",
+    "author": "Manar",
+    "depends": ["hr"],
+    "data": [
+        "views/employee_document_views.xml",
+    ],
+    "installable": True,
+    "application": False,
+}
