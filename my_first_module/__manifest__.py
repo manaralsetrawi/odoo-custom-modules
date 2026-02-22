010fb8945964502786c74caa8795657cd931bc7b
Fatima Abdali <ffooffy35@gmail.com>
2026-02-22 09:24:01 +0300

    adjusted manifest

--- my_first_module/__manifest__.py
+++ my_first_module/__manifest__.py
@@ -2,7 +2,7 @@
     'name': 'My First Module',
     'version': '1.0',
     'summary': 'Learning custom module in Odoo 17',
-    'author': 'Fatima Hasan',
+    'author': 'Fatima ',
     'depends': ['base'],
     'data': [],
     'installable': True,
