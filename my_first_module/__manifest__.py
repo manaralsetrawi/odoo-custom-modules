b883589f43ce80ded6579695781b1afc0722fb09
Fatima Abdali <ffooffy35@gmail.com>
2026-02-22 09:11:13 +0300

    add new module

---
+++ my_first_module/__manifest__.py
@@ -0,0 +1,10 @@
+{
+    'name': 'My First Module',
+    'version': '1.0',
+    'summary': 'Learning custom module in Odoo 17',
+    'author': 'Fatima Hasan',
+    'depends': ['base'],
+    'data': [],
+    'installable': True,
+    'application': True,
+}
