893c5486fedf1972f645c3dd8357dd0a8b9fbd35
Fatima Abdali <ffooffy35@gmail.com>
2026-02-24 09:27:28 +0300

    chnage color

---
+++ backend_theme/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    'name': 'Backend Theme Custom',
+    'version': '1.0',
+    'depends': ['web'],
+    'assets': {
+        'web.assets_backend': [
+            'backend_theme/static/src/scss/backend_theme.scss',
+        ],
+    },
+    'installable': True,
+}
