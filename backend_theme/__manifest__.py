8e4104bde31cc52a754aefdadd1036f2ebf3d3f1
Fatima Abdali <ffooffy35@gmail.com>
2026-02-24 10:28:26 +0300

    fix

--- backend_theme/__manifest__.py
+++ backend_theme/__manifest__.py
@@ -3,8 +3,8 @@
     'version': '1.0',
     'depends': ['web'],
     'assets': {
-        'web.assets_backend': [
-            'backend_theme/static/src/scss/backend_theme.scss',
+       'web.assets_backend': [
+        ('before', 'web/static/src/scss/primary_variables.scss', 'backend_theme/static/src/scss/backend_theme.scss'),
         ],
     },
     'installable': True,
