fbbe11b9f1afa4dc6941bc4938e6cf33e4a6d6c5
Fatima Abdali <ffooffy35@gmail.com>
2026-02-24 09:48:39 +0300

    fix colors

--- backend_theme/__manifest__.py
+++ backend_theme/__manifest__.py
@@ -4,7 +4,11 @@
     'depends': ['web'],
     'assets': {
         'web.assets_backend': [
-            'backend_theme/static/src/scss/backend_theme.scss',
+            (
+                'after',
+                'web/static/src/scss/primary_variables.scss',
+                'backend_theme/static/src/scss/backend_theme.scss',
+            ),
         ],
     },
     'installable': True,
