ce304ebb9783e74132972f337a579684a26a3e23
Fatima Abdali <ffooffy35@gmail.com>
2026-03-02 10:45:02 +0300

    add reset button + hid butttons after taking desition

--- hr_holidays_sequential_approval/models/hr_leave.py
+++ hr_holidays_sequential_approval/models/hr_leave.py
@@ -270,3 +270,20 @@ class HrLeave(models.Model):
         raise ValidationError(
             "Validation must go through the sequential approval workflow."
         )
+    
+    def action_reset_approval(self):
+        for leave in self:
+            # Only HR Manager should reset
+            if not self.env.user.has_group('hr.group_hr_manager'):
+                raise ValidationError("Only HR Manager can reset approval.")
+
+            # Reset states
+            leave.state = 'confirm'
+            leave.supervisor_state = 'pending'
+            leave.hr_state = 'pending'
+            leave.rejection_reason = False
+
+            leave.message_post(
+                body="Approval workflow has been reset to pending by HR Manager.",
+                subtype_xmlid='mail.mt_comment'
+            )
