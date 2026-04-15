b10bf6cb1854c72ac214de79645fdd8481c711ee
Manar Alsetrawi <202200412@student.polytechnic.bh>
2026-04-15 09:14:50 +0300

    removed menu

--- purchase_execution_workflow/__manifest__.py
+++ purchase_execution_workflow/__manifest__.py
@@ -12,7 +12,6 @@
         'views/account_move_views.xml',
         'views/financial_reject_wizard_views.xml',
         'views/invoice_reject_wizard_views.xml',
-        'views/procurement_execution_menu.xml',
     ],
     'assets': {
         'web.assets_backend': [
