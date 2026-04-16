6131ef9dffd76f603112cb5e776e5ec503d54bd8
Manar Alsetrawi <202200412@student.polytechnic.bh>
2026-04-16 09:04:29 +0300

    fixing an issue

--- finance_review_workflow/__manifest__.py
+++ finance_review_workflow/__manifest__.py
@@ -8,13 +8,18 @@
     'depends': ['account'],
     'data': [
         'security/security.xml',
+        'security/ir.model.access.csv',
         'views/finance_review_account_move_views.xml',
         'views/finance_invoice_kanban_views.xml',
-        'views/finance_review_assets.xml',
         'wizards/finance_wizard_views.xml',
         'report/finance_review_summary_report.xml',
         'report/finance_review_summary_template.xml',
     ],
+    'assets': {
+        'web.assets_backend': [
+            'finance_review_workflow/static/src/scss/finance_review_workflow.scss',
+        ],
+    },
     'installable': True,
     'application': False,
 }
