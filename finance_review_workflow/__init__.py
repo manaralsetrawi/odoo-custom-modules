069f6406481a0115ffd0a4fa656a10ce521d7642
Manar Alsetrawi <202200412@student.polytechnic.bh>
2026-04-16 11:35:07 +0300

    solving issue

--- finance_review_workflow/__init__.py
+++ finance_review_workflow/__init__.py
@@ -1,4 +1,2 @@
 from . import models
 from . import wizards
-from . import finance_review_account_move
-from . import finance_control_dashboard
--- finance_review_workflow/models/__init__.py
+++ finance_review_workflow/models/__init__.py
@@ -1,1 +1,2 @@
 from . import finance_review_account_move
+from . import finance_control_dashboard
