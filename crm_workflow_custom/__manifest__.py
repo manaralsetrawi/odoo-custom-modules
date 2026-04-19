3105468284c9b33c85c1eeda43420495e3f56159
Manar Alsetrawi <202200412@student.polytechnic.bh>
2026-04-19 11:28:04 +0300

    Added user roles and security groups

--- crm_workflow_custom/__manifest__.py
+++ crm_workflow_custom/__manifest__.py
@@ -3,16 +3,19 @@
     'version': '18.0.1.0.0',
     'summary': 'Custom CRM workflow, reminders, approvals, dashboards, and support integration',
     'category': 'Sales/CRM',
-    'author': 'Manar Alsetrawi - NCST',
-    'depends': [ #update later get Fatima module name 
+    'author': 'Your Name',
+    'depends': [
         'crm',
         'mail',
     ],
     'data': [
+        'security/security.xml',
+        'security/ir.model.access.csv',
         'views/crm_sequence_data.xml',
         'views/crm_stage_data.xml',
         'views/crm_lead_views.xml',
         'views/crm_support_ticket_views.xml',
+        'views/crm_reporting_views.xml',
         'views/crm_menu_views.xml',
         'data/crm_reminder_cron.xml',
     ],
