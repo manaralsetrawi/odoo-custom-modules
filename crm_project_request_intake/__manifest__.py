210345734d18ab94464a88431a575bc4ccf27dc2
Manar Alsetrawi <202200412@student.polytechnic.bh>
2026-04-20 13:44:52 +0300

    chnage the page

--- crm_project_request_intake/__manifest__.py
+++ crm_project_request_intake/__manifest__.py
@@ -29,7 +29,7 @@ Main features:
         'security/ir.model.access.csv',
         'data/crm_stage_data.xml',
         'views/crm_lead_views.xml',
-        'views/project_request_page.xml',
+        'views/website_contact_form.xml',
         'views/crm_project_request_menu.xml',
     ],
     'assets': {
