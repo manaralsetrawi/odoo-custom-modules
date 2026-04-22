82bbd446a98d1f3593aee5d8c9bede9cad5fd592
Manar Alsetrawi <202200412@student.polytechnic.bh>
2026-04-22 10:25:08 +0300

    fix

--- crm_project_request_intake/__manifest__.py
+++ crm_project_request_intake/__manifest__.py
@@ -32,7 +32,6 @@ Main features:
         'views/project_request_reject_wizard_views.xml',
         'views/website_contact_form.xml',
         'views/crm_client_segment_views.xml',
-        'views/crm_lead_tag_views.xml',
         'views/crm_project_type_views.xml',
         'views/crm_project_request_menu.xml',
     ],
