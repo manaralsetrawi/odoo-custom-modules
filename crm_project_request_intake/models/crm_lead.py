b4b872d837be762b8bb4452047a2f670d9b39f5c
FatimaAbdali <ffooffy35@gmail.com>
2026-04-22 12:05:44 +0300

    fix

--- crm_project_request_intake/models/crm_lead.py
+++ crm_project_request_intake/models/crm_lead.py
@@ -54,7 +54,7 @@ class CrmProjectRequestLead(models.Model):
     request_type = fields.Selection([
         ('general_inquiry', 'General Inquiry'),
         ('project_request', 'Project Request'),
-    ], string='Request Type', default='general_inquiry', tracking=True)
+    ], string='Request Type', default='project_request', tracking=True)
 
     # =========================================================
     # Client Submission Fields
