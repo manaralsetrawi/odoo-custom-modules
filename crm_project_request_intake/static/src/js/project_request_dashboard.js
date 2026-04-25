7052e12f7f011779c15acfc80c95a8045659acae
Manar Alsetrawi <202200412@student.polytechnic.bh>
2026-04-25 20:40:05 +0300

    test

--- crm_project_request_intake/static/src/js/project_request_dashboard.js
+++ crm_project_request_intake/static/src/js/project_request_dashboard.js
@@ -1,45 +1,45 @@
-/** @odoo-module **/
+// /** @odoo-module **/
 
-import { registry } from "@web/core/registry";
-import { Component, onWillStart, useState } from "@odoo/owl";
-import { useService } from "@web/core/utils/hooks";
+// import { registry } from "@web/core/registry";
+// import { Component, onWillStart, useState } from "@odoo/owl";
+// import { useService } from "@web/core/utils/hooks";
 
-class ProjectRequestDashboard extends Component {
-    setup() {
-        this.orm = useService("orm");
-        this.action = useService("action");
+// class ProjectRequestDashboard extends Component {
+//     setup() {
+//         this.orm = useService("orm");
+//         this.action = useService("action");
 
-        this.state = useState({
-            data: {},
-        });
+//         this.state = useState({
+//             data: {},
+//         });
 
-        onWillStart(async () => {
-            this.state.data = await this.orm.call(
-                "crm.lead",
-                "get_project_request_dashboard_data",
-                []
-            );
-        });
-    }
+//         onWillStart(async () => {
+//             this.state.data = await this.orm.call(
+//                 "crm.lead",
+//                 "get_project_request_dashboard_data",
+//                 []
+//             );
+//         });
+//     }
 
-    async openLeads(status = false, priority = false) {
-        const action = await this.orm.call(
-            "crm.lead",
-            "action_open_project_request_leads_from_dashboard",
-            [],
-            {
-                status: status,
-                priority: priority,
-            }
-        );
+//     async openLeads(status = false, priority = false) {
+//         const action = await this.orm.call(
+//             "crm.lead",
+//             "action_open_project_request_leads_from_dashboard",
+//             [],
+//             {
+//                 status: status,
+//                 priority: priority,
+//             }
+//         );
 
-        this.action.doAction(action);
-    }
-}
+//         this.action.doAction(action);
+//     }
+// }
 
-ProjectRequestDashboard.template = "crm_project_request_intake.ProjectRequestDashboard";
+// ProjectRequestDashboard.template = "crm_project_request_intake.ProjectRequestDashboard";
 
-registry.category("actions").add(
-    "project_request_dashboard_action",
-    ProjectRequestDashboard
-);
+// registry.category("actions").add(
+//     "project_request_dashboard_action",
+//     ProjectRequestDashboard
+// );
